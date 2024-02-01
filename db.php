a4b774d74e7bbc3c4d264225bf61407a8cb534c5
AlejandroCastilloBurgos <alejandrocastillo734@gmail.com>
2024-02-01 19:35:22 +0100

    actualizado todo

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+session_start();
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=tesla", $username, $password); //alimentos
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?> 
